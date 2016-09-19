5b984309eccd5c4a1ea31c44fd659c622c3ea05c
singhbikram <arcforyou@gmail.com>
2016-09-19 13:03:47 -0700

    First commit

---
+++ MVCWingtipToys/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWingtipToys.MvcApplication" Language="C#" %>
---
+++ MVCWingtipToys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWingtipToys.Startup))]
+namespace MVCWingtipToys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
